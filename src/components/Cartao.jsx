a23c5b89d9185b47fcc597c8d2d90bad27389fd7
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-14 18:28:55 -0300

    styles

--- src/components/Cartao.jsx
+++ src/components/Cartao.jsx
@@ -1,54 +1,70 @@
 import React, { useState } from 'react';
 import Cards from 'react-credit-cards';
-import { Content } from '../styles';
+import { Content, Form } from '../styles';
 
 
 export default function Cartao() {
   const [number, setNumber] = useState('');
   const [name, setName] = useState('');
   const [expiry, setExpiry] = useState('');
-  const [cvc, setCvc] = useState();
+  const [cvc, setCvc] = useState('');
+  const [focus, setFocus] = useState('');
 
   return(
     <Content>
+      <div>
         <Cards
           cvc={cvc}
           expiry={expiry}
-          //focused={focus}
+          focused={focus}
           name={name}
           number={number}
           />
+      </div>
       <div>
-        <form>
-          <input
-            type="tel"
-            name="number"
-            placeholder="Número do cartão"
-            maxLength="16"
-            onChange={(e) => setNumber(e.target.value)}
-          />
-          <input
-            type="text"
-            name="name"
-            placeholder="Nome"
-            maxLength="45"
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="number"
-            name="expiry"
-            placeholder="Validade"
-            maxLength="4"
-            onChange={(e) => setExpiry(e.target.value)}
-          />
-          <input
-            type="number"
-            name="cvc"
-            placeholder="CVC"
-            maxLength="3"
-            onChange={(e) => setCvc(e.target.value)}
-          />
-        </form>
+        <Form>
+          <div>
+            <input
+              type="number"
+              name="number"
+              placeholder="Número do cartão"
+              value={number}
+              maxLength="16"
+              onChange={(e) => setNumber(e.target.value)}
+              onFocus={(e) => setFocus(e.target.name)}
+              />
+            <input
+              type="text"
+              name="name"
+              value={name}
+              placeholder="Nome"
+              maxLength="45"
+              onChange={(e) => setName(e.target.value)}
+              onFocus={(e) => setFocus(e.target.name)}
+              />    
+          </div>
+          <div>
+            <input
+              type="number"
+              name="expiry"
+              placeholder="Validade 'MM/AA'"
+              value={expiry}
+              maxLength="4"
+              onChange={(e) => setExpiry(e.target.value)}
+              onFocus={(e) => setFocus(e.target.name)}
+              />
+            <input
+              type="number"
+              name="cvc"
+              placeholder="CVC"
+              value={cvc}
+              maxLength="3"
+              onChange={(e) => setCvc(e.target.value)}
+              onFocus={(e) => setFocus(e.target.name)}
+            />
+          </div>
+          <button type="button">Pagar</button>
+        </Form>
       </div>
     </Content>
   )
--- src/styles/index.js
+++ src/styles/index.js
@@ -5,11 +5,60 @@ export const Header = styled.header`
   display: flex;
   flex-direction: column;
   color: rgb(28,28,28);
-  margin: auto 2rem;
+  margin: 0 2rem;
+
+  @media (min-width: 768px) {
+    margin: 3rem 2rem;
+
+    p {
+      font-size: 20px;
+    }
+  }
 `;
 
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
   margin: 2rem;
+  padding: 1.5rem;
+  border: 2px solid gray;
+  border-radius: 10px;
+
+  @media (min-width: 768px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin: auto;
+    width: 80%;
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 1rem;
+  width: 100%;
+
+  input, button {
+    margin-bottom: 0.5rem;
+    background-color: gainsboro;
+    border-radius: 5px;
+    padding: 0.4rem;
+    font-size: 1rem;
+    width: 70%;
+  }
+
+  button {
+    width: 50%;
+    color: white;
+    background-color: mediumseagreen;
+  }
+
+  @media (min-width: 768px) {
+    input:hover {
+    background-color: silver;
+    }
+
+    button:hover {
+    background-color: mediumaquamarine;
+    }
+  }
 `;
